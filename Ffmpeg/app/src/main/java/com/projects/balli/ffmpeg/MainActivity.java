cca3987e7ab940ed2f600cbb224f5aaafd12a631
vbbphc <balloli.vb@gmail.com>
2018-11-12 02:22:30 +0530

    Adds: Asset executable hack code

--- Ffmpeg/app/src/main/java/com/projects/balli/ffmpeg/MainActivity.java
+++ Ffmpeg/app/src/main/java/com/projects/balli/ffmpeg/MainActivity.java
@@ -1,9 +1,12 @@
 package com.projects.balli.ffmpeg;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,17 +16,17 @@ import android.widget.Toast;
 
 import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
-import com.github.hiteshsondhi88.libffmpeg.FFmpegLoadBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
 import com.jaredrummler.android.shell.CommandResult;
 import com.jaredrummler.android.shell.Shell;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
@@ -36,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     FFmpeg ffmpeg;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,95 +72,35 @@ public class MainActivity extends AppCompatActivity {
                     new String[]{Manifest.permission.READ_CONTACTS},
                     CAMERA_PERMISSION);
         } else {
-            String localPath = this.getFilesDir().getAbsolutePath();
-            Log.e(TAG, "onCreate: Local path" + localPath );
-            for (File f : File.) {
-                Log.e(TAG, "Files: " + f.getName());
-                Log.e(TAG, "Can exec: " + f.canExecute() );
-            }
-
-            CommandResult result = Shell.SH.run("/data/local/tmp/Vectors/SHM-12.4/bin/TAppEncoderStaticd -c");
+            importExecutables();
+            String path = "/storage/emulated/0/";
+            String command = "320 240 " + path +"output_test_420.yuv 160 120 " + path + "output_test_420_Q.yuv";
+            CommandResult result = Shell.SH.run("/data/data/com.projects.balli.ffmpeg/files/DownConvertStaticd " + command);
             if (result.isSuccessful()) {
-                Log.e(TAG, "onCreate: Result " + result.getStdout() );
+                Log.e(TAG, "onCreate: Shell output " + result.getStdout() );
             } else {
-                Log.e(TAG, "onCreate: Unsuccess " );
-                Log.e(TAG, "onCreate: Error " + result.getStderr() );
+                Log.e(TAG, "onCreate: Shell failed " + result.getStderr() );
             }
 
-//            try{
-//                StringBuffer output = new StringBuffer();
-//                Process p = Runtime.getRuntime().exec("ls " + localPath);
-//                p.waitFor();
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//                String line = "";
-//                while ((line = reader.readLine())!= null) {
-//                    output.append(line + "n");
-//                }
-//                Log.e(TAG, "onCreate: output" + output.toString() );
-//            }catch(IOException | InterruptedException e){
-//                Log.e(TAG, "onCreate: Process error " + e.getMessage() );
-//            }
 
-//            try {
-//                Log.e(TAG, "onCreate: Running copy stuff" );
-//                Process sh = Runtime.getRuntime().exec("cp -r /sdcard/Vectors " + localPath);
-//                OutputStream os = sh.getOutputStream();
-//                os.write(("cd /data/user/0/com.projects.balli.ffmpeg/files && ls").getBytes("ASCII"));
-//                os.flush();
-//                os.close();
-//                sh.waitFor();
-//            } catch (IOException | InterruptedException e) {
-//                Log.e(TAG, "onCreate: Exception occured" );
-//                Log.e(TAG, "onCreate: " + e.getMessage() );
-//                e.printStackTrace();
-//            }
 
-//            try {
-//                Process sh = Runtime.getRuntime().exec("su", null, null);
-//                OutputStream os = sh.getOutputStream();
-//                os.write(("chmod 666 /dev/video0").getBytes("ASCII"));
-//                os.flush();
-//                os.close();
-//                sh.waitFor();
-//            } catch (InterruptedException | IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            String vectorsPath = Environment.getExternalStorageDirectory().getAbsolutePath();
-//
-//            String inputFile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + "/input_file.mp4";
-//            //String inputFile = "/dev/video0";
-//            String outputFile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + "/output_420.yuv";
-//
+            String inputFile = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DCIM).getAbsolutePath() + "/test.mp4";
+            String outputFile = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DCIM).getAbsolutePath() + "/output_test_420.yuv";
+            Log.e(TAG, "onCreate: " + inputFile );
 //            //String command = "-f v4l2 -framerate 0.25 -video_size 320x240 -i " + inputFile + " -vframes 449 " + outputFile;
-//            String command = "-i " + inputFile + " -s 320x240 -r 0.25 -frames:v 449 -pix_fmt yuv420p " + outputFile;
-//
+            //String command = "-i " + inputFile + " -s 320x240 -r 0.25 -frames:v 449 -pix_fmt yuv420p " + outputFile;
+
 //            try {
-//                Log.e(TAG, "onCreate: " + "process started" );
-//                Process sh = Runtime.getRuntime().exec("");
-//                DataOutputStream outputStream = new DataOutputStream(sh.getOutputStream());
-//
-//                outputStream.writeBytes("/sdcard/Vectors/SHM-12.4/bin/TAppEncoderStaticd -c /sdcard/Vectors/server/cfg/raScale.cfg -c \n");
-//                outputStream.flush();
-//
-//                outputStream.writeBytes("exit\n");
-//                outputStream.flush();
-//                sh.waitFor();
-//            } catch (IOException e) {
-//                Log.e(TAG, "onCreate: " + "process exception" );
-//                e.printStackTrace();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            try {
-//                run(ffmpeg, command);
+//                run(ffmpeg, command, "YUV420 Command");
 //            } catch (FFmpegCommandAlreadyRunningException e) {
 //                e.printStackTrace();
 //            }
         }
     }
 
-    private void run(FFmpeg fFmpeg, String command) throws FFmpegCommandAlreadyRunningException {
+    private void run(FFmpeg fFmpeg, String command, final String logMessage) throws FFmpegCommandAlreadyRunningException {
         String[] ffmpeg_cmd = command.split(" ");
         fFmpeg.execute(ffmpeg_cmd, new ExecuteBinaryResponseHandler() {
 
@@ -169,20 +113,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgress(String message) {
                 super.onProgress(message);
-                Log.e(TAG, "onProgress: " + message );
+                Log.e(TAG, "onProgress: " + logMessage + " " + message );
             }
 
             @Override
             public void onSuccess(String message) {
                 super.onSuccess(message);
-                makeToast("Success");
+                Log.e(TAG, "onSuccess: " + logMessage + " " + message );
 
             }
 
             @Override
             public void onFailure(String message) {
                 super.onFailure(message);
-                Log.e(TAG, "onFailure: " + message );
+                Log.e(TAG, "onFailure: " + logMessage + " " + message );
                 makeToast("Fail");
             }
 
@@ -225,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
                     //String command = "-f v4l2 -framerate 0.25 -video_size 320x240 -i " + inputFile + " -vframes 449 " + outputFile;
                     String command = "-i " + inputFile + " -t 30 -s 320x240 -r 0.25 -frames:v 449 " + outputFile;
                     try {
-                        run(ffmpeg, command);
+                        run(ffmpeg, command, "YUV 420");
                     } catch (FFmpegCommandAlreadyRunningException e) {
                         e.printStackTrace();
                     }
@@ -238,6 +182,27 @@ public class MainActivity extends AppCompatActivity {
 
             // other 'case' lines to check for other
             // permissions this app might request.
+        }
+    }
+
+    private void importExecutables() {
+        try {
+            InputStream ins = getAssets().open("DownConvertStaticd");
+            byte[] buffer = new byte[ins.available()];
+            ins.read(buffer);
+            ins.close();
+            FileOutputStream fos = this.openFileOutput("DownConvertStaticd", Context.MODE_PRIVATE);
+            fos.write(buffer);
+            fos.close();
+
+            File file = getFileStreamPath ("DownConvertStaticd");
+            file.setExecutable(true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.e(TAG, "importExecutables: File not found error " );
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "importExecutables: IO exception" );
         }
     }
 }
