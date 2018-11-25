7c766c9bcc3dd1180112d8092942d0b03f10dc05
vbbphc <f2016165@hyderabad.bits-pilani.ac.in>
2018-11-25 14:28:11 +0530

    Adds: Receiver table

--- Vec2/app/src/main/java/in/swifiic/vec2/SenderActivity.java
+++ Vec2/app/src/main/java/in/swifiic/vec2/SenderActivity.java
@@ -282,7 +282,7 @@ public class SenderActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sender);
 
         createVideoFolder();
         createImageFolder();
